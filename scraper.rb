d63641fc34cdb0e2499bb9a602ae2c79c0ec737c
Henare Degan <henare.degan@gmail.com>
2016-05-04 15:01:07 +1000

    Write a scraper to save speaker details

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,20 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require "json"
+require "open-uri"
+require "scraperwiki"
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+url = "https://raw.githubusercontent.com/g0v/summit.g0v.tw/2016/app/javascripts/components/SpeakerList/speakers.json"
+speakers = JSON.parse(open(url).read)["en-US"]
+avatar_base_url = "https://raw.githubusercontent.com/g0v/summit.g0v.tw/2016/app/javascripts/components/SpeakerList/"
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+speakers.map! do |s|
+  # Use full YouTube URL
+  s["interview"] = "https://youtu.be/" + s["interview"] if s["interview"]
+  # Make avatar URL complete if it's relative
+  s["avatar"] = avatar_base_url + s["avatar"][2..-1] if s["avatar"] && s["avatar"][0..1] == "./"
+  # Change boolean so it can be saved in SQLite
+  s["featured"] = 1 if s["featured"]
+
+  s
+end
+
+ScraperWiki.save_sqlite(["name"], speakers)
